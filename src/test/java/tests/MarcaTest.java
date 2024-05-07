ebdc913219aa1a0e9149d99be0c88d4c30a97534
Alejandra230701 <aalvarti@emeal.nttdata.com>
2024-05-07 10:23:05 +0200

    Change framework

---
+++ src/test/java/tests/MarcaTest.java
@@ -0,0 +1,67 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static utilities.BasePage.waitSeconds;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import pages.Marca;
+import utilities.*;
+
+@ExtendWith(TestErrorHandler.class)
+public class MarcaTest {
+
+  private static Marca controller;
+
+  @BeforeAll
+  public static void clean_reports_logs() {
+    JSExecutor.runCommand(LocalEnviroment.isWindows() ? Constants.ALLURE_CLEAN_COMMAND_WIN : Constants.ALLURE_CLEAN_COMMAND_MAC);
+    JSExecutor.runCommand(LocalEnviroment.isWindows() ? Constants.NETWORK_LOG_CLEAN_COMMAND_WIN : Constants.NETWORK_LOG_CLEAN_COMMAND_MAC);
+    if (LocalEnviroment.isAndroid()) JSExecutor.runCommand(Constants.NETWORK_LOG_CLEAN_COMMAND_ANDROID);
+  }
+
+  @BeforeEach
+  public void iAmOnTheMarcaWebsite() {
+    controller = new Marca();
+  }
+
+  @Test
+  public void checkNewsArticleImage() {
+    controller.acceptCookies();
+  }
+
+  @Test
+  public void checkIfNewExists() {
+    controller.acceptCookies();
+    controller.goToNotice();
+    assertTrue(true, "La imagen no se muestra en pantalla");
+  }
+
+  @AfterEach
+  public void closeDriver() {
+    Accessibility.checkAccessibility();
+    AllureReport.fillReportInfo();
+    NetworkLogs.getNetworkLogs();
+    waitSeconds(Constants.LOW_TIMEOUT);
+  }
+
+  @AfterAll
+  public static void runReports() {
+    runAllureReport();
+    runAccessibilityCopy();
+  }
+
+  public static void runAllureReport() {
+    if (LocalEnviroment.isWindows()) {
+      JSExecutor.runCommand(Constants.ALLURE_COMMAND_WIN);
+    } else {
+      JSExecutor.runCommand(Constants.ALLURE_COMMAND_MAC);
+    }
+  }
+
+  public static void runAccessibilityCopy() {
+    if (LocalEnviroment.getAccessibility() && LocalEnviroment.isWeb()) {
+      Accessibility.moveHtmlReportToAccessibilityDirectory(Constants.ACCESSIBILITY_REPORT_PATH);
+    }
+  }
+}
