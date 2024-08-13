c28d3271852dd27d6cfb0642cca196efd27ee6da
VladSntt <vladalexandru.simion.st@emeal.nttdata.com>
2024-08-13 14:43:37 +0200

    Reformat y fw version (#6)
    * Reformat y fw version
    
    * fw version

--- src/main/java/pages/Marca.java
+++ src/main/java/pages/Marca.java
@@ -6,7 +6,6 @@ import static utilities.DriverConfiguration.getDriver;
 import io.appium.java_client.pagefactory.AndroidFindBy;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
 import java.time.Duration;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
